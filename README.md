5a3b9b1383ef32221a713537975ee4ab6e742a7f
Onni Hakala <hakala.onni@gmail.com>
2014-04-25 13:46:09 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Git to wordpress plugin svn pusher
+======================
+Easily push your wordpress plugin from git repo into svn.
+Remember versionnumbers in `readme.txt` and `your_plugin.php`
+
+##Usage
+
+1.Clone this project
+2.Add your plugin git repo to `/git` folder
+3.checkout your wordpress svn into `/svn`
+4.Rename plugin.php file in `push-git-to-svn.sh`
+5.Run script: `./push-git-to-svn.sh`
---
+++ push-git-to-svn.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#Check these variables accordingly to your plugin
+
+PLUGINNAME="your_plugin_name.php" #CHANGE THIS LINE FOR YOUR FILE <============================
+GITDIR="git"
+SVNDIR="svn"
+
+#!/bin/bash
+cd "$PWD/$GITDIR"
+
+git status
+# warn if there are uncommitted changes!
+
+git pull origin master
+
+# Check version in readme.txt is the same as plugin file
+NEWVERSION1=`grep "^Stable tag" readme.txt | awk -F' ' '{print $3}'`
+echo "readme.txt version: $NEWVERSION1"
+NEWVERSION2=`grep "^Version" $PLUGINNAME | awk -F' ' '{print $2}'`
+echo "$PLUGINNAME version: $NEWVERSION2"
+if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
+echo "Versions match in readme.txt and PHP file. Let's proceed..."
+
+# sed to bump version?
+# git commit -am "Bumped version up"
+
+git tag -f -s $NEWVERSION1 -m "Tagged to match version in SVN"
+
+echo "Pushing git master to origin, with tags"
+git push origin master
+git push origin master --tags
+
+cd ../$SVNDIR/
+
+svn update
+
+# use rsync --delete to sync all add/delete changes?
+cp ../$GITDIR/* ./trunk
+svn add trunk --force
+svn ci -m "Sync with git"
+
+# Git tag -> SVN-tag
+svn copy trunk tags/$NEWVERSION1
+svn ci -m "Tagged version $NEWVERSION1"
