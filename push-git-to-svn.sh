9f6bd982ef16965224dd956f6aa641ca5c553fa1
Otto Kekäläinen <otto@seravo.fi>
2015-05-09 16:16:35 +0300

    Committed my own version

--- push-git-to-svn.sh
+++ push-git-to-svn.sh
@@ -1,31 +1,58 @@
 #!/bin/bash
+# This scripts expects to be run in a directory where
+# wp-plugin-x.git and wp-plugin-x.svn
+# exists and can be pushed and pulled.
+# This script also assumes certain layout and contents
+# of WP plugins according strictly to the official WP standards.
 
-#Check these variables accordingly to your plugin
+set -e # Stop on errors
 
-PLUGINNAME="your-plugin-name.php" #CHANGE THIS LINE FOR YOUR PLUGIN
-#============================
-GITDIR="git"
-SVNDIR="svn"
+if [[ -z "$@" ]]; then
+    echo >&2 "You must supply an argument!"
+    echo "Example: ./push-git-to-svn wp-https-domain-alias.git"
+    exit 1
+elif [[ ! -d "$@/.git" ]] && [[ ! -d "$@.git/.git" ]]; then
+    echo >&2 "$@ is not a valid git repository!"
+    exit 1
+fi
 
-#!/bin/bash
-cd "$PWD/$GITDIR"
+PLUGINNAME=`echo $@ | cut -d '.' -f 1`
+echo "Syncing plugin $PLUGINNAME to wordpress.org"
+
+cd $PLUGINNAME.git
 
 git status
 # warn if there are uncommitted changes!
 
-git pull origin master
+git pull
 
 # Check version in readme.txt is the same as plugin file
 NEWVERSION1=`grep "^Stable tag" readme.txt | awk -F' ' '{print $3}'`
 echo "readme.txt version: $NEWVERSION1"
-NEWVERSION2=`grep "Version" $PLUGINNAME | awk -F' ' '{print $3}'`
-echo "$PLUGINNAME version: $NEWVERSION2"
-if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't 
-match. Exiting...."; exit 1; fi
+
+if [[ -f $PLUGINNAME.php ]]; then
+  NEWVERSION2=`grep "Version" $PLUGINNAME.php | awk -F' ' '{print $3}'`
+  echo "$PLUGINNAME.php version: $NEWVERSION2"
+elif [[ -f `echo $PLUGINNAME | cut -d - -f 2-`.php ]]; then
+  FILENAME=`echo $PLUGINNAME | cut -d - -f 2-`.php
+  NEWVERSION2=`grep "Version" $FILENAME | awk -F' ' '{print $3}'`
+  echo "$FILENAME version: $NEWVERSION2"
+else
+  echo "Plugin main php file not found. Exiting...."
+  exit 1
+fi
+
+if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then
+  echo "Versions don't match. Exiting...."
+  exit 1
+fi
+
 echo "Versions match in readme.txt and PHP file. Let's proceed..."
 
-# sed to bump version?
-# git commit -am "Bumped version up"
+if [[ -d "../$PLUGINNAME.svn/tags/$NEWVERSION1" ]]; then
+  echo "This tag already exists in SVN. Bump version number before upload. Exiting.."
+  exit 1
+fi
 
 git tag -f -s $NEWVERSION1 -m "Tagged to match version in SVN"
 
@@ -33,15 +60,18 @@ echo "Pushing git master to origin, with tags"
 git push origin master
 git push origin master --tags
 
-cd ../$SVNDIR/
 
+cd ../$PLUGINNAME.svn/trunk
 svn update
 
 # use rsync --delete to sync all add/delete changes?
-cp -r ../$GITDIR/* trunk/
-svn add trunk --force
+cp ../../$PLUGINNAME.git/* .
+svn add -q *
+# Note: if a file is removed from git, it will not
+# be automatically removed from svn.
 svn ci -m "Sync with git"
 
 # Git tag -> SVN-tag
-svn copy trunk tags/$NEWVERSION1
+cd ..
+svn cp trunk tags/$NEWVERSION1
 svn ci -m "Tagged version $NEWVERSION1"
